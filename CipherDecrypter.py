4119863d2874c6def2d19574ee4dbea468e8aee5
Musheebat <98224710+musheebat@users.noreply.github.com>
2022-06-23 13:29:32 -0400

    Add files via upload

---
+++ CipherDecrypter.py
@@ -0,0 +1,15 @@
+#Cipher-Decrypter
+# Author: Subhash
+# Title: Offensive Penetration Testing Student
+# Organisation: HackerAssociate
+#Usage : On plain text : put plain text 
+# On encrypted text: put cipher text
+
+plaintext = "Plain-text-message"
+ciphertext = "encrypted-text"
+key = ""
+for i in range(len(plaintext)):
+ num_key = ((ord(ciphertext[i]) - ord(plaintext[i])) % 26) + 97
+ char_key = chr(num_key)
+ key = key + char_key
+print key
